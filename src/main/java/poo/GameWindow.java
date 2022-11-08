2f4f04f66de34152ea018f80647fc232cc4c8005
Lorran Marques <lorran9963@gmail.com>
2022-11-07 22:19:01 -0300

    init

---
+++ src/main/java/poo/GameWindow.java
@@ -0,0 +1,116 @@
+package poo;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class GameWindow extends Application implements GameListener {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+		Game.getInstance().addGameListener(this);
+
+		primaryStage.setTitle("Batalha de Cartas");
+
+		GridPane grid = new GridPane();
+		grid.setAlignment(Pos.CENTER);
+		grid.setHgap(20);
+		grid.setVgap(20);
+		grid.setPadding(new Insets(25, 25, 25, 25));
+
+		DeckView deckJ1 = new DeckView(1);
+		DeckView deckJ3 = new DeckView(3);
+		DeckView deckJ5 = new DeckView(5);
+		ScrollPane sd1 = new ScrollPane();
+		ScrollPane sd3 = new ScrollPane();
+		ScrollPane sd5 = new ScrollPane();
+		sd1.setPrefSize(600, 128);
+		sd1.setContent(deckJ1);
+		grid.add(sd1, 1, 0);
+		sd3.setPrefSize(600, 128);
+		sd3.setContent(deckJ3);
+		grid.add(sd3, 1, 1);
+		sd5.setPrefSize(600, 128);
+		sd5.setContent(deckJ5);
+		grid.add(sd5, 1, 2);
+
+		PlacarView placar = new PlacarView();
+		grid.add(placar, 0, 3);
+
+		Button butClean = new Button("Clean");
+		grid.add(butClean, 2, 3);
+		butClean.setOnAction(e -> Game.getInstance().removeSelected());
+
+		DeckView deckJ2 = new DeckView(2);
+		DeckView deckJ4 = new DeckView(4);
+		DeckView deckJ6 = new DeckView(6);
+		ScrollPane sd2 = new ScrollPane();
+		ScrollPane sd4 = new ScrollPane();
+		ScrollPane sd6 = new ScrollPane();
+		sd2.setPrefSize(600, 128);
+		sd2.setContent(deckJ2);
+		grid.add(sd2, 1, 4);
+		sd4.setPrefSize(600, 128);
+		sd4.setContent(deckJ4);
+		grid.add(sd4, 1, 5);
+		sd6.setPrefSize(600, 128);
+		sd6.setContent(deckJ6);
+		grid.add(sd6, 1, 6);
+
+		Scene scene = new Scene(grid);
+
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
+	@Override
+	public void notify(GameEvent eg) {
+		Alert alert;
+		if (eg == null) return;
+		if (eg.getTarget() == GameEvent.Target.GWIN) {
+			switch (eg.getAction()) {
+			case INVPLAY:
+				alert = new Alert(AlertType.WARNING);
+				alert.setTitle("Atenção !!");
+				alert.setHeaderText("Jogada inválida!!");
+				alert.setContentText("Era a vez do jogador " + eg.getArg());
+				alert.showAndWait();
+				break;
+			case MUSTCLEAN:
+				alert = new Alert(AlertType.WARNING);
+				alert.setTitle("Atenção !!");
+				alert.setHeaderText(null);
+				alert.setContentText("Utilize o botao \"Clean\"");
+				alert.showAndWait();
+				break;
+			case ENDGAME:
+				String text = "Fim de Jogo !!\n";
+				if (Game.getInstance().getPtsJ1() > Game.getInstance().getPtsJ2()) {
+					text += "O jogador 1 ganhou !! :-)";
+				} else {
+					text += "O jogador 2 ganhou !! :-)";
+				}
+				alert = new Alert(AlertType.WARNING);
+				alert.setTitle("Parabens !!");
+				alert.setHeaderText(null);
+				alert.setContentText(text);
+				alert.showAndWait();
+				break;
+			case REMOVESEL:
+				// Esse evento não vem para cá
+			}
+		}
+	}
+
+}
