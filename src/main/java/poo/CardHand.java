b835aa0476b143e852f61cf716f80f9e4218f2ee
Lorran Marques <lorran9963@gmail.com>
2022-11-10 21:50:48 -0300

    TentativaLeituraCSV

--- src/main/java/poo/CardHand.java
+++ src/main/java/poo/CardHand.java
@@ -1,10 +1,18 @@
 package poo;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class CardHand {
 	public static final int NCARDS = 5;
@@ -16,6 +24,17 @@ public class CardHand {
 		cartas = new ArrayList<>(NCARDS);
 		selected = null;
 		Random r = new Random();
+		String caminhoAtual = Paths.get("D:\\OneDrive - PUCRS - BR\\Faculdade\\POO\\yugioh-java\\src\\main\\resources\\imagens\\KaibaDeck.csv").toAbsolutePath().toString();
+        Path caminho = Paths.get(caminhoAtual);
+		try (Scanner sc = new Scanner(Files.newBufferedReader(caminho, Charset.defaultCharset()))) {
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+				System.out.print(line);
+            }
+        } catch (IOException x) {
+            System.err.format("Erro de E/S: %s%n", x);
+        }
+		
 		for (int i = 0; i < NCARDS; i++) {
 			int n = r.nextInt(10) + 1;
 			Card c = new Card("C" + n, "img" + n, n);
