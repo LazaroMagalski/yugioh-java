551f30de46aabdddbf33a637d05082d99ae3a31d
Lázaro Magalski <lazaro.magalski@smartconsulting.com.br>
2022-11-26 21:44:46 -0300

    alterado o nextplayer retorno int

--- src/main/java/yugiohgame/Views/PlacarView.java
+++ src/main/java/yugiohgame/Views/PlacarView.java
@@ -10,7 +10,8 @@ import yugiohgame.Events.GameEvent;
 import yugiohgame.Listeners.GameListener;
 
 public class PlacarView extends GridPane implements GameListener {
-	private TextField ptsJ1, ptsJ2, jogador;
+	private TextField ptsJ1, ptsJ2;
+	private Label lbJ1, lbJ2;
 
 	public PlacarView() {
 		this.setAlignment(Pos.CENTER);
@@ -22,25 +23,31 @@ public class PlacarView extends GridPane implements GameListener {
 
 		ptsJ1 = new TextField();
 		ptsJ2 = new TextField();
-		jogador = new TextField();
 
 		ptsJ1.setText("" + Game.getInstance().getPtsJ1());
 		ptsJ2.setText("" + Game.getInstance().getPtsJ2());
-		String numJog = Integer.toString(Game.getInstance().nextPlayer());
-		jogador.setText("" + numJog);
+		lbJ1 = new Label("LP Jogador 1:");
+		lbJ2 = new Label("LP Jogador 2:");
 
-		this.add(new Label("LP Jogador 1:"), 0, 0);
+		this.add(lbJ1, 0, 0);
 		this.add(ptsJ1, 1, 0);
-		this.add(new Label("LP Jogador 2:"), 0, 1);
+		this.add(lbJ2, 0, 1);
 		this.add(ptsJ2, 1, 1);
-		this.add(new Label("Jogador da Rodada"), 0, 2);
-		this.add(jogador, 1, 2);
+		lbJ1.setStyle("-fx-font-weight: bold");
+		lbJ2.setStyle("-fx-font-weight: regular");
 	}
 
 	@Override
 	public void notify(GameEvent event) {
 		ptsJ1.setText("" + Game.getInstance().getPtsJ1());
 		ptsJ2.setText("" + Game.getInstance().getPtsJ2());
-		jogador.setText("" + Integer.toString(Game.getInstance().nextPlayer()));
+		if(Game.getInstance().nextPlayer()==1){
+			lbJ1.setStyle("-fx-font-weight: bold");
+			lbJ2.setStyle("-fx-font-weight: regular");
+		}
+		else{
+			lbJ1.setStyle("-fx-font-weight: regular");
+			lbJ2.setStyle("-fx-font-weight: bold");
+		}
 	}
 }
